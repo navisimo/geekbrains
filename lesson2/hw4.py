051846bb74c9c658f2759fc74c4907aec817ee16
navisimo <47449816+navisimo@users.noreply.github.com>
2021-02-05 19:47:23 +0300

    Update hw4.py
    fix code

--- lesson2/hw4.py
+++ lesson2/hw4.py
@@ -2,7 +2,6 @@ phrase = input("введите строку: ")
 new_list = phrase.split()
 count = 1
 for el in new_list:
-    number = new_list.index(el)
     if len(el) > 10:
         el = el[:10]
     print(f'строка №{count}, слово - {el}')
