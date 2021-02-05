4abc5fbdd3f9dc0e8f0ea654cfc15782a6fc9911
navisimo <47449816+navisimo@users.noreply.github.com>
2021-02-05 19:44:41 +0300

    Update hw4.py
    fix code

--- lesson2/hw4.py
+++ lesson2/hw4.py
@@ -1,7 +1,9 @@
 phrase = input("введите строку: ")
 new_list = phrase.split()
+count = 1
 for el in new_list:
     number = new_list.index(el)
     if len(el) > 10:
         el = el[:10]
-    print(f'строка №{number + 1}, слово - {el}')
+    print(f'строка №{count}, слово - {el}')
+    count += 1
